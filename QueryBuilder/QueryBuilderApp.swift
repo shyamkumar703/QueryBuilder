29ccd0089079d7d6adf66ccb593dffa7e03a3d72
Shyam Kumar <shyamkumar@berkeley.edu>
2023-03-25 12:28:20 -0700

    Initial Commit

---
+++ QueryBuilder/QueryBuilderApp.swift
@@ -0,0 +1,17 @@
+//
+//  QueryBuilderApp.swift
+//  QueryBuilder
+//
+//  Created by Shyam Kumar on 3/25/23.
+//
+
+import SwiftUI
+
+@main
+struct QueryBuilderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
